0999a342a7573ea5372e87d157a31632b0d33650
Julia Magnusson <juliamagnuss@gmail.com>
2021-12-10 14:00:45 +0100

    exprimenterar med lånad kod från grupparbete

--- forum/index.js
+++ forum/index.js
@@ -8,7 +8,47 @@ app.listen(4000);
 console.log("Kör servern på localhost:4000"); // bara dubbelkollar att servern körs
 
 
-// Skapar en route till startsidan
+
 app.get("/",(req,res) => {  
   res.sendFile(__dirname + "/form.html");
 });
+
+
+
+app.get("/hamta-data", (req, res) => { 
+    console.log("mottog förfrågan från klienten"); // bara dubbelkollar att allt fungerar som det ska
+    fs.readFile("meddelanden.json", function(err, data) {
+      if (err) throw err;
+      
+      res.send(data);
+    });
+  });
+  
+
+  app.get("/skriv-fran-mall", (req, res) => { 
+    fs.readFile("form.html", function(err, data){
+        fs.readFile("meddelanden.json", function(err, myJson) {
+            res.send(data.toString());
+        });
+    });
+  });
+  
+  
+ 
+  app.post("/skriv-fran-mall", (req, res) => {
+  
+    
+    fs.readFile("form.html", function(err, data){
+    fs.readFile("meddelanden.json", function (err, myJson) {
+      
+        let json = JSON.parse(myJson);
+        let newPost = {
+          name: req.body.name,
+          message: req.body.message ,
+          email: req.body.email
+        };
+
+    });
+});
+
+  
