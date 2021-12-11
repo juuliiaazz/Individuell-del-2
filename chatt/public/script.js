d63b69dd39775aa2cc61dcce59fcbd39454f8561
Julia Magnusson <juliamagnuss@gmail.com>
2021-12-11 12:59:58 +0100

    lagt till kommentarer

--- chatt/public/script.js
+++ chatt/public/script.js
@@ -1,29 +1,34 @@
+// OBS! Jag har valt att använda mig av live server-extension för att chatten ska uppdateras live utan att hela sidan laddas om.
+// Se till att du har "live server"-extension för att kunna köra programmet, annars fungerar det inte.
+// Öppna sedan chatten genom att högerklicka på index.html och öppna i live servern. Öppnar du flera fönster får du fler personer som är aktiva i chatten samtidigt
+
+
 const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-
+// frågar användaren vad den vill bli kallad i chatten genom att skicka en prompt med en input-ruta
 const name = prompt('Vad heter du?')
-appendMessage('Du anslöt till chatten')
+appendMessage('Du anslöt till chatten') // Skickar ett meddelande i din chatt att du anslutit
 socket.emit('new-user', name)
 
-
+// gör att ditt/andras användarnamn + meddelande syns i chatten
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
 
-
+// berättar vilka personer som anslöt till chatten
 socket.on('user-connected', name => {
   appendMessage(`${name} anslöt till chatten`)
 })
 
-
+// berättar om någon lämnat chatten och vem denne var
 socket.on('user-disconnected', name => {
   appendMessage(`${name} lämnade chatten`)
 })
 
-
+// När du trycker på skicka-knappen kommer både du och andra att se ditt meddelande. För dig står det "Du: xxx" för andra står det "*ditt namn*: xxx"
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
--- chatt/server.js
+++ chatt/server.js
@@ -1,7 +1,11 @@
+// OBS! Jag har valt att använda mig av live server-extension för att chatten ska uppdateras live utan att hela sidan laddas om.
+// Se till att du har "live server"-extension för att kunna köra programmet, annars fungerar det inte.
+// Öppna sedan chatten genom att högerklicka på index.html och öppna i live servern. Öppnar du flera fönster får du fler personer som är aktiva i chatten samtidigt
+
 const io = require('socket.io')(3000)
 
 const users = {}
-
+// för att kunna visa att personer ansluts, skickar meddelande och lämnar chatten
 io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
